196eaa15837109e81f99cb2bdacdfb8d23d45851
yuchunfeng <yuchunfeng@xiguakeji.net>
2017-09-25 15:01:03 +0800

    超级管理员登录接口

---
+++ api/application/system/controller/Login.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\system\controller;
+
+/*******超级管理员后台接口*******/
+/*******登录，注册接口*******/
+class Login{
+	// 登录
+	public function index(){
+		echo 1;
+	}
+	//注册
+	function register(){
+		
+	}
+} 
+ ?>
