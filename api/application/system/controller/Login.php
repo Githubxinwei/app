176506e04b5e6fd8ffdad36d5769eb1c0c8212b7
李佳飞 <lijiafei@xiguakeji.net>
2017-09-25 18:30:01 +0800

    测试登录的接口验证

--- api/application/system/controller/Login.php
+++ api/application/system/controller/Login.php
@@ -8,8 +8,8 @@ class Login{
      * 登录请求接口
      */
 	public function index(){
-		$username = input('get.username','','htmlspecialchars');
-		$password = input('get.password','','htmlspecialchars');
+		$username = input('post.username','','htmlspecialchars');
+		$password = input('post.password','','htmlspecialchars');
 
 		if(!$username || !$password){
 		    $arr['code'] = 10001;$arr['msg'] = '账号或密码为空';$arr['msg_test'] = '账号或密码为空';
@@ -21,9 +21,9 @@ class Login{
             return json($arr);
         }
         if($info[0]['password'] == xgmd5($password)){
-		    $data['lastIp '] = $info[0]['nowIp'];
+		    $data['lastIp'] = $info[0]['nowIp'];
 		    $data['nowIp'] = $_SERVER["REMOTE_ADDR"];
-		    $data['lastTime'] = $info[0]['nowTime '];
+		    $data['lastTime'] = $info[0]['nowTime'];
 		    $data['nowTime'] = strtotime('Y年m月d日H:i:s',time());
             $res = db('system') -> where('username',$username) -> update($data);
             if($res){
