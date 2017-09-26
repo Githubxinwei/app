07eff50aaa66e4e2811f32ddfd90e68e71b7d40c
李佳飞 <lijiafei@xiguakeji.net>
2017-09-26 11:07:51 +0800

    添加总后台的设置四个功能

---
+++ api/application/system/model/SystemPay.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/9/26 0026
+ * Time: 09:20
+ */
+namespace app\system\model;
+use think\Model;
+
+class SystemPay extends Model{
+
+}
---
+++ api/application/system/model/SystemSite.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/9/26 0026
+ * Time: 10:19
+ */
+namespace app\system\model;
+use think\Model;
+
+class SystemSite extends Model{
+
+}
