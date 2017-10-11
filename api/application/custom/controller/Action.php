25601a2fcab485c75585a1d90352a502918b5337
yuchunfeng <yuchunfeng@xiguakeji.net>
2017-10-11 10:15:09 +0800

    修改临时通信票据方式

--- api/application/custom/controller/Action.php
+++ api/application/custom/controller/Action.php
@@ -5,6 +5,15 @@ class Action extends Controller{
 
 	function _initialize(){
 //		$this->custom = model('custom') -> where('id',2) -> find();
+		$this -> data = input("post.",'','htmlspecialchars');
+		if(!isset($this->data['session_key'])){
+			$return['code'] = 0;
+			$return['msg'] = '登录超时，请重新登录';
+			$return['msg_test'] = '请登录';
+			echo json_encode($return);exit;
+		}
+		
+		session_id($this->data['session_key']);
 		$this->custom = session('custom');
 		if(!$this->custom){
 			$return['code'] = 0;
@@ -12,7 +21,8 @@ class Action extends Controller{
 			$return['msg_test'] = '请登录';
 			echo json_encode($return);exit;
 		}
-		$this->custom = db('custom') -> find($this->custom->id);
+		$this->custom = model('custom') -> where('id',$this->custom['id']) -> find();
+		
 	}
 }
 
