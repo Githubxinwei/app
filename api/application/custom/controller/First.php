0843a5ed1c32261f1a1c8be6d5d6d87de281ca74
songyanyan <songyanyan@xiguakeji.net>
2017-10-24 17:57:23 +0800

    预约 基本信息 分类

--- api/application/custom/controller/First.php
+++ api/application/custom/controller/First.php
@@ -77,7 +77,7 @@ class First{
 
 // 忘记密码
    public  function  forget(){
-       
+
 	   $username = $username = input('post.username','','htmlspecialchars');
        $password = input('post.password','','htmlspecialchars');
 
@@ -106,7 +106,6 @@ class First{
        }
 
    }
-
 
 }
 
