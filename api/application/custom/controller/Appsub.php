1fea183c8b5de164c1cbd25ce743a5014f449d4c
songyanyan <songyanyan@xiguakeji.net>
2017-10-27 11:10:55 +0800

    预约前台接口添加

--- api/application/custom/controller/Appsub.php
+++ api/application/custom/controller/Appsub.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Administrator
+ * User: 宋妍妍
  * Date: 2017/10/27 0027
  * Time: 09:16
  */
@@ -12,7 +12,7 @@ use think\Controller;
 class Appsub  extends Xiguakeji
 {
 
-//获取bannar信息
+   /*获取bannar信息 appid*/
     function get_bannar(){
 
         $info = db('loop_img') -> where('appid',$this->apps) -> find();
@@ -26,11 +26,12 @@ class Appsub  extends Xiguakeji
         return json($return);
     }
 
-    //获取预约列表
+    /*获取预约列表 appid*/
     function lists(){
 
         $keyword = isset($this->data['keyword']) ? $this->data['keyword'] : '';
         if($keyword){$where['name'] = array('like','%'.$keyword.'%');}
+        if(isset($this->data['cid'])) $where[]=['exp',"FIND_IN_SET(".$this->data['cid'].",cid)"];
         $where['appid'] = $this->apps;
         if(isset($this->data['page'])){$page = $this->data['page'];}else{$page = 1;}
         if(isset($this->data['limit_num'])){$limit_num = $this->data['limit_num'];}else{$limit_num = 20;}
@@ -46,7 +47,7 @@ class Appsub  extends Xiguakeji
 
     }
 
-   //预约获取分类
+   /*预约获取分类 appid*/
    public  function  subcate(){
 
        $info = db('subscribe_cate')-> field('id,name') -> where('appid',$this->apps) -> order('code desc') -> select();
@@ -55,6 +56,20 @@ class Appsub  extends Xiguakeji
 
 
    }
+
+   /*服务人员列表  appid */
+    public  function  getuser(){
+
+        $info = db('subscribe_service_user')-> field('id,name,desc,pic') -> where('appid',$this->apps) -> order('id desc') -> select();
+        $return['code'] = 10000;
+        $return['data'] = $info;
+        return json($return);
+
+    }
+
+    /*分类下的服务列表*/
+
+
 
 
 
