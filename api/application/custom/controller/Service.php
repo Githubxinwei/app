558749c3a0062c13971324c0670f3f4a4ff0f6b9
xinwei <xinwei@xiguakeji.net>
2017-10-27 09:29:39 +0800

    服务项目和预约设置修改

--- api/application/custom/controller/Service.php
+++ api/application/custom/controller/Service.php
@@ -88,7 +88,7 @@ class Service extends Action{
 	        }
 	    }
 	    
-	    $res = db('subscribe_service') -> allowField(true) -> where(['id' =>$this -> data['service_id'],'custom_id' => $this->custom->id])->save($this->data);
+	    $res = model('subscribe_service') -> allowField(true) -> save(['id' =>$this -> data['service_id'],'custom_id' => $this->custom->id]);
 	    $return['code'] = 10000;$return['msg'] = '修改成功'.$this->custom->id;
 	   
 	    return json($return);
